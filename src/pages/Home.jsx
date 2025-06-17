a0a215b5d81d39bae84ddda12fcc85a78e774909
SafawanaHossain <143733969+SafawanaHossain@users.noreply.github.com>
2025-06-17 16:52:11 -0400

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Home = () => {
-    return(
+    return (
+    <div>
         <p>Home</p>
-    );
+        <p>Welcome to the oas site!</p>
+    </div>
+);
 }
 
 export default Home;
